9765a9a5986623e29bd9861171e765878492b558
Kelar89 <kingshisha.indonesia@gmail.com>
2025-07-17 06:41:27 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,12 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
-<<<<<<< HEAD
-  eleventyConfig.addPassthroughCopy("admin"); // <-- PASTIKAN BARIS INI ADA
-
-
-
-=======
   eleventyConfig.addPassthroughCopy("admin");
-  
->>>>>>> 1223facfc41ea1b243d6e776d17b3cfe26a16d43
+
   return {
+    // Bagian pathPrefix inilah yang akan memperbaiki semua link Anda
+    pathPrefix: "/porto/", 
+
     dir: {
       input: ".",
       includes: "_includes",
